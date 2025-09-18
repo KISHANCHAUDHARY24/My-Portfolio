2f4405ca3f539070c79e3a7f3edd3375ba54516e
Kishan Chaudhary <kritatyaghya@gmail.com>
2025-09-18 17:59:33 +0545

    This is my first commit of Portfolio to github

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Home,{Body,Contact} from './Portfolio/Home'
+import Portfolio from './Portfolio'
+
+
+const App = () => {
+  return (
+    <div>
+      <Portfolio></Portfolio>
+    </div>
+  )
+}
+
+export default App
+
